39f9fd7dd471fed3c366e93e31214c5f3b046707
Anvesh Muppeda <muppedaanvesh@gmail.com>
2024-09-28 18:59:19 -0500

    feature: Adding echo pod name dockerfile

---
+++ dockerfiles/echo-pod/pod_name.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the hostname (which is the pod name in Kubernetes)
+POD_NAME=$(hostname)
+
+# Get the node name
+NODE_NAME=$(cat /etc/hostname)
+
+# Replace the placeholders in the HTML with the actual pod name and node name
+sed -i "s/{{POD_NAME}}/${POD_NAME}/g" /usr/share/nginx/html/index.html
+sed -i "s/{{NODE_NAME}}/${NODE_NAME}/g" /usr/share/nginx/html/index.html
+
+# Start Nginx
+nginx -g 'daemon off;'
